7532e099d3befa5815ca5677ab2fc9ae92362842
David Orimoyegun <dorimoyegun@outlook.com>
2023-09-02 23:24:30 +0100

    created navbar and added styling

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import './style.css';
+import initialiseWebpage from './webpage';
+
+initialiseWebpage();
---
+++ src/webpage.js
@@ -0,0 +1,91 @@
+import weatherIcon from './assets/weather-icon.svg';
+
+function setActiveButton(button) {
+  const buttons = document.querySelectorAll('.button-container__unit-button');
+
+  buttons.forEach((eachbutton) => {
+    if (eachbutton !== this) {
+      eachbutton.classList.remove('active-unit-button');
+    }
+  });
+
+  button.classList.add('active-unit-button');
+}
+
+function createNavbar() {
+  const weatherLogo = new Image();
+  weatherLogo.src = weatherIcon;
+  weatherLogo.classList.add('image-container__weather-logo');
+
+  const imageContainer = document.createElement('div');
+  imageContainer.classList.add('left-nav__image-container');
+  imageContainer.appendChild(weatherLogo);
+
+  const iconElement = document.createElement('i');
+  iconElement.classList.add('fa-solid', 'fa-magnifying-glass');
+  iconElement.setAttribute('id', 'searchbaricon');
+
+  const searchIcon = document.createElement('div');
+  searchIcon.classList.add('search-bar-container__search-icon');
+  searchIcon.appendChild(iconElement);
+
+  const inputElement = document.createElement('input');
+  inputElement.setAttribute('type', 'text');
+  inputElement.classList.add('search-bar-container__search-bar');
+  inputElement.setAttribute('id', 'search-bar');
+  inputElement.setAttribute('placeholder', 'Search location...');
+
+  const searchBarContainer = document.createElement('div');
+  searchBarContainer.classList.add('left-nav__search-bar-container');
+  searchBarContainer.appendChild(inputElement);
+  searchBarContainer.appendChild(searchIcon);
+
+  const leftNavContainer = document.createElement('div');
+  leftNavContainer.classList.add('nav__left-nav');
+  leftNavContainer.appendChild(imageContainer);
+  leftNavContainer.appendChild(searchBarContainer);
+
+  const celciusButton = document.createElement('button');
+  celciusButton.classList.add(
+    'button-container__unit-button',
+    'button-container--celcius-button',
+    'active-unit-button',
+  );
+  celciusButton.textContent = '°C,mm,m/s';
+  celciusButton.addEventListener('click', (e) => {
+    if (e.target.classList.contains('active')) return;
+    setActiveButton(celciusButton);
+  });
+
+  const fahrenheitButton = document.createElement('button');
+  fahrenheitButton.classList.add(
+    'button-container__unit-button',
+    'button-container--fahrenheit-button',
+  );
+  fahrenheitButton.textContent = '°F,in,mph';
+  fahrenheitButton.addEventListener('click', (e) => {
+    if (e.target.classList.contains('active')) return;
+    setActiveButton(fahrenheitButton);
+  });
+
+  const buttonContainer = document.createElement('div');
+  buttonContainer.classList.add('right-nav__button-container');
+  buttonContainer.appendChild(celciusButton);
+  buttonContainer.appendChild(fahrenheitButton);
+
+  const rightNavContainer = document.createElement('div');
+  rightNavContainer.classList.add('nav__right-nav');
+  rightNavContainer.appendChild(buttonContainer);
+
+  const nav = document.createElement('nav');
+  nav.classList.add('nav');
+  nav.appendChild(leftNavContainer);
+  nav.appendChild(rightNavContainer);
+
+  return nav;
+}
+
+export default function initialiseWebpage() {
+  const container = document.querySelector('.container');
+  container.appendChild(createNavbar());
+}
